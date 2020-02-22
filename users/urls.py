341cdd52993ba7e4f5d2985d2a7b3f30a4cab228
flo-schu <fluncki@protonmail.com>
2020-02-22 12:37:46 +0100

    manage login function on website

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 
 from django.contrib.auth import views
 
+from users.views import Login
+
 app_name = 'users'
 
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(template_name='users/login.html'), name = 'login'),
+    path('login/', Login.as_view(), name = 'login'),
+    path('password_reset/', views.PasswordResetView.as_view() , name = 'password_reset'),
+    path('logout/', views.LogoutView.as_view(), name = 'logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.views import LoginView, PasswordResetView
+
+
+class Login(LoginView):
+    template_name = "users/login.html"
