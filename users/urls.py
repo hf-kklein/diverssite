dd0b8e633b1d227407fbd48c1e5d1a88480e102b
flo-schu <fluncki@protonmail.com>
2020-03-11 22:51:38 +0100

    registration / signup form

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from django.contrib.auth import views
 
-from users.views import Login
+from users.views import Login, SignUp
 
 app_name = 'users'
 
@@ -11,5 +11,5 @@ urlpatterns = [
     path('login/', Login.as_view(), name = 'login'),
     path('password_reset/', views.PasswordResetView.as_view() , name = 'password_reset'),
     path('logout/', views.LogoutView.as_view(template_name= 'users/logout.html'), name = 'logout'),
-    # path('register/', views.
+    path('signup/', SignUp.as_view(template_name='users/signup.html'), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.views import LoginView, PasswordResetView
-
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 class Login(LoginView):
     template_name = "users/login.html"
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'signup.html'
