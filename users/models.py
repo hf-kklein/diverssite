5b1eed3844f1a340757728741fe28ebc517117ad
flo-schu <fluncki@protonmail.com>
2020-03-11 23:06:47 +0100

    begin user profile page

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.PROTECT)
+    firstname = models.CharField(max_length=50, null=True)
+    lastname = models.CharField(max_length=50, null=True)
+    gender = models.CharField(choices=(("d","divers"),
+                                       ("f","female"),
+                                       ("m","male")), max_length= 10)
+    trikotnummer = models.CharField(max_length=3, null=True)
+    email = models.EmailField()
+    street = models.CharField(max_length=50, null=True)
+    place = models.CharField(max_length=50, null=True)
+    zip = models.CharField(max_length=50, null=True)
