099273c7db0d3bf6db02553dd03d58a40ea6d711
flo-schu <fluncki@protonmail.com>
2020-06-14 23:41:16 +0200

    update url from category to categ

--- events/urls.py
+++ events/urls.py
@@ -7,7 +7,7 @@ app_name = 'events'
 urlpatterns = [
     # path('', views.IndexView.as_view(), name='index'),
     path('', views.IndexView.as_view(), name = "index"),
-    path('<slug:slug>/', views.IndexView.as_view(), name='category'),
+    path('<slug:slug>/', views.IndexView.as_view(), name='categ'),
 
     # path('<int:event_id>/', views.detail, name='detail'),
 ]
