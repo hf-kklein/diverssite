85edf383930f4be1c84f4e335fa6e7116312b080
flo-schu <fluncki@protonmail.com>
2020-06-14 11:28:41 +0200

    add requirements.txt

--- diverssite/settings.py
+++ diverssite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("django_secret_key")#'byw6o%go0wqtz0endj$b$141_u5nb61744jg)e2yqvq5#!9i$b'
+SECRET_KEY = os.environ.get("django_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
