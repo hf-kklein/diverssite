cc95ba9dc205ff7901a7dd97a3272c678a99b932
flo-schu <fluncki@protonmail.com>
2021-07-22 00:19:21 +0200

    add counts and participation to events

--- events/views.py
+++ events/views.py
@@ -1,102 +1,19 @@
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.urls import reverse
-from django.views import generic, View
+from django.views import  View
 from django.utils import timezone
 from django.forms import formset_factory
 from django.contrib.auth.models import User
-import json
 import datetime as dt
 
 from .forms import EventForm
 from .models import Event, Participation, PartChoice, Categ
 from wiki.models import Article, Display
 
-class EventsViewOld(View):
-    """
-    add some standard methods to all views related to events.
-    """
-    def nest_dict(self,flat):
-        result = {}
-        for k, v in flat.items():
-            self._nest_dict_rec(k, v, result)
-        return result
-
-    def _nest_dict_rec(self,k, v, out):
-        k, *rest = k.split('_', 1)
-        if rest:
-            self._nest_dict_rec(rest[0], v, out.setdefault(k, {}))
-        else:
-            out[k] = v
-
-    def create_events_dict(self):
-        # print(self.cats)
-        start_today = datetime.datetime.combine(datetime.datetime.today(), datetime.time(0, 0, 0))
-        subset_events = Event.objects.filter(categ__in=self.cats)
-        event_query = subset_events.filter(date__gte=timezone.make_aware(start_today)).order_by('date')
-        choice_query = PartChoice.objects.all()
-        choice_yes = PartChoice.objects.get(choice = 'y')
-        choice_no = PartChoice.objects.get(choice = 'n')
-        choice_maybe = PartChoice.objects.get(choice = 'm')
-
-        events = dict()
-        for event in event_query:
-            participation_all = event.participation_set.all()
-            participation_yes = participation_all.filter(part = choice_yes)
-            participation_no = participation_all.filter(part = choice_no)
-            participation_maybe = participation_all.filter(part = choice_maybe)
-            part_count = len(participation_all.filter(part = choice_yes))
-            try:
-                participation_user = event.participation_set.get(person = self.user_query).part.choicetext
-            except:
-                participation_user = None
-
-            choices = dict()
-            for i in choice_query:
-                choices[i.choice] = {'choicetext': i.choicetext,
-                                     'userchoice': participation_user}
-
-            events[event.id] = {'event': event,
-                                'participation_all': participation_all,
-                                'participation_yes': participation_yes,
-                                'participation_no': participation_no,
-                                'participation_maybe': participation_maybe,
-                                'participation_count': part_count,
-                                'participation_user': participation_user,
-                                'choices': choices}
-
-
-        return events
-
-    def process_participation(self, data):
-        evlist = data["evlist"]
-        print(evlist)
-        for u in evlist:
-            for ev in evlist[u]:
-                use = User.objects.get(username=u)
-                print(use)
-                eve = Event.objects.get(pk=ev)
-                cho = PartChoice.objects.get(choice=evlist[u][ev][0])
-                p = Participation.objects.filter(person=use)
-                p = p.filter(event=eve)
-                if len(p) == 0:
-                    pnew = Participation(
-                        event = eve,
-                        person = use,
-                        part = cho
-                        )
-                    pnew.save()
-                elif len(p) == 1:
-                    p.update(part=cho)
-                else:
-                    print("error. Too many events selected.")
-                    break
-
 def get_categ(slug):
     if slug != None:
         return Categ.objects.filter(slug=slug)
-        # print(cat)
-        # self.cats = self.allcats.filter(categ=sel_cat)
     else:
         return Categ.objects.all()
 
@@ -109,16 +26,24 @@ def query_events(slug):
 def query_participation(user, events):
     # create participation objects if they do not exist for user-event combis
     participants = []
+    girls = []
+    boys = []
+    divers = []
     for e in events:
         # can be done with get_or_create()
         try:
-            participants.append(Participation.objects.filter(event=e))
+            party = Participation.objects.filter(event=e)
+            # party = sorted(party, key=lambda p: p.part.pk)
+            girls.append([p for p in party if p.person.profile.gender == "f"])
+            boys.append([p for p in party if p.person.profile.gender == "m"])
+            divers.append([p for p in party if p.person.profile.gender == "d"])
+            participants.append(party)
             part = Participation.objects.get(event=e, person=user)
         except Participation.DoesNotExist:
             Participation(event=e, person=user).save()
 
     return Participation.objects.filter(event__in=events) \
-        .filter(person=user), participants
+        .filter(person=user), participants, girls, boys, divers
     
 class IndexView(View):
     template_name = 'events/eventslist.html'
@@ -126,8 +51,15 @@ class IndexView(View):
 
     def get(self, request, slug=None):
         events = query_events(slug)
-        participation, participants = query_participation(request.user, events)
-        particount = [len(p) for p in participants]
+        participation, participants, girls, boys, divers = query_participation(
+            request.user, events)
+        gcount = [[p for p in party if p.part.choice == "y"]
+                  for party in girls]
+        bcount = [[p for p in party if p.part.choice == "y"]
+                  for party in boys]
+        dcount = [[p for p in party if p.part.choice == "y"]
+                  for party in divers]
+                  
         self.info.update({"particip": [{
             "id":p.id,
             "event":p.event_id, 
@@ -147,7 +79,9 @@ class IndexView(View):
 
         context = { 'posts':  posts,
                     'formset': forms,
-                    'eventforms': zip(events, forms, participants, particount),
+                    'eventforms': zip(
+                        events, forms, participants, girls, boys, divers,
+                        gcount, bcount, dcount),
                     'user': request.user,
                     'categories': get_categ(None)}
 
