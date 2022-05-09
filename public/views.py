1c6d029532faa7c6e2b6d596a9086d74064285e3
flo-schu <fluncki@protonmail.com>
2022-05-09 23:59:59 +0200

    minor formatting and removal of unneeded code in public/views.py

--- public/views.py
+++ public/views.py
@@ -3,7 +3,7 @@ import datetime
 from django.utils import timezone
 from django.views import generic
 
-from events.models import Categ, Event, PartChoice
+from events.models import Event, PartChoice
 from public.models import Info
 from wiki.models import Article, Display
 
@@ -19,19 +19,23 @@ class IndexView(generic.ListView):
         posts = Article.objects.filter(show_on_pages=home)
         public_posts = posts.filter(visibility="public")
         published_posts = public_posts.filter(pub_date__lte=timezone.now())
-        start_today = datetime.datetime.combine(datetime.datetime.today(), datetime.time(0, 0, 0))
+        start_today = datetime.datetime.combine(
+            datetime.datetime.today(), datetime.time(0, 0, 0))
         yes = PartChoice.objects.filter(choice="y")[0]
-        next_events = Event.objects.all().filter(date__gte=timezone.make_aware(start_today)).order_by("date")
+        next_events = Event.objects.all() \
+            .filter(date__gte=timezone.make_aware(start_today)) \
+            .order_by("date")
         if len(next_events) > 0:
             next_training = next_events[0]
             participants = next_training.participation_set.all()
             coming = [p for p in participants if p.part == yes]
-            partn = sum([1 for p in coming])
         else:
-            next_training = {"date": "Sommer: Di/Do, 18 Uhr ATV; " "Winter: Mo/Do, 22 Uhr HTWK", "name": ""}
+            next_training = {
+                "date": "Sommer: Di/Do, 18 Uhr ATV; " "Winter: Mo/Do, 22 Uhr HTWK", 
+                "name": ""
+            }
             participants = None
             coming = []
-            partn = 0
 
         context = {
             "welcome": welcome,
@@ -39,6 +43,5 @@ class IndexView(generic.ListView):
             "next_training": next_training,
             "participants": participants,
             "coming": coming,
-            "partn": partn,
         }
         return context
