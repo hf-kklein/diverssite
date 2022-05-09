877937642e87f2576528ba1ee200c165e17fcbb3
flo-schu <fluncki@protonmail.com>
2022-05-09 11:03:22 +0200

    merge pull request and change fix some new errors in sidebar
    - improved styling
    - include all events
    - add link to events

--- public/views.py
+++ public/views.py
@@ -3,7 +3,7 @@ import datetime
 from django.utils import timezone
 from django.views import generic
 
-from events.models import Categ, Event, PartChoice, Participation
+from events.models import Categ, Event, PartChoice
 from public.models import Info
 from wiki.models import Article, Display
 
@@ -20,19 +20,19 @@ class IndexView(generic.ListView):
         public_posts = posts.filter(visibility="public")
         published_posts = public_posts.filter(pub_date__lte=timezone.now())
         start_today = datetime.datetime.combine(datetime.datetime.today(), datetime.time(0, 0, 0))
-        training = Categ.objects.get(name="training")
         yes = PartChoice.objects.filter(choice="y")[0]
-        next_trainings = (
-            Event.objects.filter(categ=training).order_by("-date").filter(date__gte=timezone.make_aware(start_today))
+        next_events = (
+            Event.objects.all().filter(date__gte=timezone.make_aware(start_today)).order_by("date")
         )
-        if len(next_trainings) > 0:
-            next_training = next_trainings[0]
+        if len(next_events) > 0:
+            next_training = next_events[0]
             participants = next_training.participation_set.all()
             coming = [p for p in participants if p.part == yes]
             partn = sum([1 for p in coming])
         else:
-            next_training = {"date": "Sommer: Di/Do, 18 Uhr ATV; " "Winter: Mo/Do, 22 Uhr HTWK"}
+            next_training = {"date": "Sommer: Di/Do, 18 Uhr ATV; " "Winter: Mo/Do, 22 Uhr HTWK", "name": ""}
             participants = None
+            coming = []
             partn = 0
 
         context = {
