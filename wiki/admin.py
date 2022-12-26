40ae954c520d2d398da81ceac5017b5d26937185
flo-schu <fluncki@protonmail.com>
2022-12-26 23:53:22 +0100

    implement image and file upload in wiki
    - image and file get their own models to store all objects
    - objects are referenced in wiki articles
    - objects can also be created in wiki articles
    - add images in detail html
    - use tabularInline to upload and select images and files in ArticleAdmin
    - create form for admin to specify widgets for fields
    - create various functions to retrieve urls
    - upload static images for file types

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,23 +1,30 @@
 from django.contrib import admin
-from django.db import models
-from django.forms.widgets import CheckboxSelectMultiple
 from simple_history.admin import SimpleHistoryAdmin
 
 # Register your models here.
-from .models import Article, Category, Display
+from .models import Article, Category, Display, Image, File
+from .forms import ArticleAdminForm
 
+class FileTabularInline(admin.TabularInline):
+    extra = 1
 
+class FileInline(FileTabularInline):
+    model = Article.files.through
+
+class ImageInline(FileTabularInline):
+    model = Article.images.through
+
+@admin.register(Article)
 class ArticleAdmin(SimpleHistoryAdmin):
+    form = ArticleAdminForm
+    inlines = [ImageInline, FileInline]
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ("title", "visibility", "pub_date")
+    list_display = ("title", "visibility", "pub_date", )
     list_filter = ["pub_date", "visibility", "show_on_pages"]
     search_fields = ["title"]
+    exclude = ["images", "files"]
 
-    formfield_overrides = {
-        models.ManyToManyField: {"widget": CheckboxSelectMultiple},
-    }
-
-
+admin.site.register(Image)
 admin.site.register(Category)
-admin.site.register(Article, ArticleAdmin)
 admin.site.register(Display)
+admin.site.register(File)
--- wiki/models.py
+++ wiki/models.py
@@ -1,12 +1,12 @@
+import os
 from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
 from simple_history.models import HistoricalRecords
-
 # Create your models here.
-
+from events.models import Event
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -26,24 +26,58 @@ vis_choice = (("public", "Public"), ("members", "Members"))
 
 
 def file_directory_path(instance, filename):
+    try:
+        event = instance.event.name
+        date = instance.event.date.strftime("%Y%m%d")
+    except AttributeError:
+        event = ""
+        date = instance.date.strftime("%Y%m%d")
+
+    title = "unnamed" if instance.title == "" else instance.title
+    public = "public" if instance.public else "private"
+
     slug = slugify("-".join([
         type(instance).__name__, 
-        instance.title,
-        instance.date.strftime("%Y%m%d") + 
-        instance.time.strftime("%H%M%S%f")
+        event,
+        date,
+        title
     ]))
+
+    similar_files = [i.file.name for i in Image.objects.all() if slug in i.file.name]
+    
+    slug = slug + "-" + str(len(similar_files))
+
     ext = filename.split(".")[-1]
-    return f"wiki/{slug}.{ext}"
+    return f"{public}/wiki/{slug}.{ext}"
 
 
 # https://stackoverflow.com/questions/34006994/how-to-upload-multiple-images-to-a-blog-post-in-django/34007383
 # hier gehts weiter
 class Image(models.Model):
     title = models.CharField(max_length=20, default=None, blank=True)
+    event = models.ForeignKey(Event, blank=True, null=True, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     time = models.TimeField(auto_now_add=True)
-    image = models.ImageField(upload_to=file_directory_path, verbose_name="Image")
+    file = models.ImageField(upload_to=file_directory_path, verbose_name="Image")
+    public = models.BooleanField(default=False)
     # history = HistoricalRecords()
+
+    def __str__(self):
+        return os.path.basename(self.file.path)
+
+
+    def image_url(self):
+        """
+        Returns the URL of the image associated with this Object.
+        If an image hasn't been uploaded yet, it returns a stock image
+
+        :returns: str -- the image url
+
+        """
+        if self.file and hasattr(self.file, "url"):
+            return self.file.url
+        else:
+            return "/static/images/default_profile.png"
 
 
 class File(models.Model):
@@ -51,7 +85,37 @@ class File(models.Model):
     date = models.DateField(auto_now_add=True)
     time = models.TimeField(auto_now_add=True)
     file = models.FileField(upload_to=file_directory_path, verbose_name="File")
+    public = models.BooleanField(default=False)
+    
     # history = HistoricalRecords()
+
+    def __str__(self):
+        return os.path.basename(self.file.path)
+
+
+    def file_url(self):
+        if self.file and hasattr(self.file, "url"):
+            return self.file.url
+        else:
+            raise FileNotFoundError
+
+    def file_type_icon(self):
+        if self.file and hasattr(self.file, "url"):
+            url = self.file.url
+            if ".xlsx" in url or ".xls" in url:
+                return "/static/images/excel.png"
+
+            if ".docx" in url or ".doc" in url:
+                return "/static/images/word.png"
+            
+            if ".pdf" in url:
+                return "/static/images/pdf.png"
+            
+            else:
+                return "/static/images/document.png"
+
+        else:
+            raise FileNotFoundError
 
 
 class Article(models.Model):
