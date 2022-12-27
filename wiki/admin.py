219ff1daddbc2bb35b4ac21af478fd9380d3adfb
flo-schu <fluncki@protonmail.com>
2022-12-27 02:52:43 +0100

    jump back from edit to wiki page

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
-
+from django.http import HttpResponseRedirect
 # Register your models here.
 from .models import Article, Category, Display, Image, File
 from .forms import ArticleAdminForm
-
 
 class FileTabularInline(admin.TabularInline):
     extra = 1
@@ -32,6 +31,25 @@ class ArticleAdmin(SimpleHistoryAdmin):
     search_fields = ["title"]
     exclude = ["images", "files"]
 
+
+    # def response_add(self, request, obj, post_url_continue=None):
+    #     """
+    #     This makes the response after adding go to another 
+    #     app's changelist for some model
+    #     """
+    #     app = obj.__module__.split(".")[0]
+    #     path = f"{app}/{obj.slug}"
+    #     return redirect(path)
+
+
+    def response_change(self, request, obj, post_url_continue=None):
+        """
+        This makes the response go to the newly created
+        model's change page without using reverse
+        """
+        _, redirect_page = request.get_full_path().split("?next=")
+
+        return HttpResponseRedirect(redirect_page)
 
 admin.site.register(Image)
 admin.site.register(Category)
